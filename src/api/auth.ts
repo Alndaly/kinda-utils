9938d282af36d8ad69fc8d68c51421ddf86d07b0
华勤达 <hqd01334772@antgroup.com>
2022-11-09 18:02:12 +0800

    docs: ✏️ 增加函数说明

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,24 @@
 import { post } from "../utils/request";
 
-export const getToken = async (url: string, body: any = {}, clientInfo: any) => {
+
+interface TokenGetApiBody {
+    grant_type: string;
+    username: string;
+    password: string;
+}
+interface ClientInfo {
+    client_id: string;
+    client_secret: string;
+}
+
+/**
+ * 
+ * @param url 获取token的接口链接
+ * @param body 含有用户信息的传参
+ * @param clientInfo 含有客户端信息的传参
+ * @returns 
+ */
+export const getToken = async (url: string, body: TokenGetApiBody, clientInfo: ClientInfo) => {
     const {
         client_id,
         client_secret
